6c4f4d1563dff628eb58ffdea3faf2ce9f75594b
Muntaha <muntahakamal15@gmail.com>
2020-11-23 13:52:17 +0530

    .

--- Naive_Bayes_Classifier.py
+++ Naive_Bayes_Classifier.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pandas as pd 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
+#reading data from csv file
 dataset =  pd.read_csv('D:\ELLAssDataset\health_data.csv')
 X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1:]
-datasize = X.shape[0]
+y = dataset.iloc[:,-1:].values
+
+datasize = dataset.shape[0]
+X_train = X[:(datasize*7)//10,:]
+y_train = y[:(datasize*7)//10,:]
+X_test = X[(datasize*7)//10:,:]
+y_test = y[(datasize*7)//10:,:]
+
+
